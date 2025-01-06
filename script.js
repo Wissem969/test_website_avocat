fcc198509e77ea0bbbc6ee65971c4ea07d53fca1
Wissem969 <127847003+Wissem969@users.noreply.github.com>
2025-01-06 15:39:29 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,9 @@
-document.getElementById('contact-form').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevents the page from reloading
-
-    // Get the form data
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
-    const topic = document.getElementById('topic').value;
-    const message = document.getElementById('message').value;
-
-    // Here, you can integrate with an API to send this data to a server
-
-    // Show the thank-you message
-    const responseMessage = document.getElementById('response-message');
-    responseMessage.textContent = `Thank you, ${name}! We have received your request about ${topic} and will get back to you at ${email} soon.`;
-
-    // Optionally, clear the form after submission
-    document.getElementById('contact-form').reset();
-});
+// Smooth Scroll for CTA buttons
+document.getElementById('cta-btn').addEventListener('click', function () {
+    window.location.href = "#pricing";
+  });
+  
+  document.getElementById('cta-btn-pricing').addEventListener('click', function () {
+    window.location.href = "#services";
+  });
+  
